60820b09949c1f23a657fabcd268cd685a828948
freetonik <freetonik@users.noreply.github.com>
2025-10-04 00:22:29 +0000

    Deploying to gh-pages from @ iqm-finland/sdk@05b45c9b4fe301fd15acacce2198cc786231401a 🚀

--- iqm-exa-common/_static/documentation_options.js
+++ iqm-exa-common/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '27.1.0',
+    VERSION: '27.2.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- iqm-pulse/_static/documentation_options.js
+++ iqm-pulse/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '12.2.0',
+    VERSION: '12.3.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
